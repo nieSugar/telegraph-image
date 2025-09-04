76bb8943dbd68cc63ea38083fcc662083a1e80c0
NieRiFeng <17673376360@163.com>
2025-09-04 18:02:22 +0800

    add cloudflare package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import "./App.css";
-import { Grommet } from "grommet";
+import { Grommet, FileInput } from "grommet";
+import CloudFlare from "cloudflare";
+
+const client = new CloudFlare({
+  apiToken: import.meta.env.CLOUDFLARE_API_TOKEN,
+});
+
 const theme = {
   global: {
     font: {
@@ -10,10 +16,23 @@ const theme = {
   },
 };
 
+const fileChange = (
+  _event: React.ChangeEvent<HTMLInputElement>,
+  { files }: { files: File[] }
+) => {
+  console.log(files, "files");
+};
+
+const fileInputMessage = {
+  browse: "点击",
+  dropPrompt: "拖拽文件到此处或",
+  dropPromptLight: "拖拽文件到此处",
+};
+
 function App() {
   return (
     <Grommet full theme={theme}>
-      <title>React App</title>
+      <FileInput messages={fileInputMessage} onChange={fileChange}></FileInput>
     </Grommet>
   );
 }
